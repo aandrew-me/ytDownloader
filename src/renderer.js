4797adcf88659eef0d5053f9ffa71f4e5fe4dca3
aandrew-me <ahammadandrew@protonmail.com>
2022-08-28 00:01:20 +0600

    App already working but still much to do

--- src/renderer.js
+++ src/renderer.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const cp = require("child_process");
 const os = require("os");
 const ffmpeg = require("ffmpeg-static");
+const YTDlpWrap = require("yt-dlp-wrap").default;
+const ytdlp = new YTDlpWrap("yt-dlp");
 const path = require("path");
 const { shell, ipcRenderer, clipboard } = require("electron");
 
@@ -15,7 +17,7 @@ fs.mkdirSync(tempDir, { recursive: true });
 let downloadDir = "";
 
 // Global variables
-let title, onlyvideo;
+let title, onlyvideo, id, thumbnail;
 
 function getId(id) {
 	return document.getElementById(id);
@@ -35,20 +37,6 @@ fs.mkdir(downloadDir, { recursive: true }, () => {});
 fs.readdirSync(tempDir).forEach((f) =>
 	fs.rmdir(`${tempDir}/${f}`, { recursive: true }, () => {})
 );
-
-// Collecting info from youtube
-// async function getVideoInfo(url) {
-// 	let info;
-// 	await ytdl
-// 		.getInfo(url)
-// 		.then((data) => {
-// 			info = data;
-// 		})
-// 		.catch((error) => {
-// 			console.log(error);
-// 		});
-// 	return info;
-// }
 
 function defaultVideoToggle() {
 	videoToggle.style.backgroundColor = "var(--box-toggleOn)";
@@ -94,6 +82,8 @@ async function getInfo(url) {
 
 		if (info) {
 			title = info.title;
+			id = info.id;
+			thumbnail = info.thumbnail
 			const formats = info.formats;
 			console.log(formats);
 
@@ -108,15 +98,15 @@ async function getInfo(url) {
 			getId("videoList").style.display = "block";
 			videoToggle.style.backgroundColor = "rgb(67, 212, 164)";
 
-			let highestQualityLength = 0;
 			let audioSize = 0;
 
 			// Getting approx size of audio file
 			for (let format of formats) {
 				if (
-					format.audio_ext !== "none" || format.acodec !== "none" &&
-					format.video_ext === "none"
+					format.audio_ext !== "none" ||
+					(format.acodec !== "none" && format.video_ext === "none")
 				) {
+					onlyvideo = true;
 					audioSize =
 						Number(format.filesize || format.filesize_approx) /
 						1000000;
@@ -135,13 +125,13 @@ async function getInfo(url) {
 				) {
 					size = (Number(size) + 0 || Number(audioSize)).toFixed(2);
 					size = size + " MB";
-					const format_id = format.format_id;
+					const format_id = format.format_id + "|" + format.ext;
 
 					const element =
 						"<option value='" +
 						format_id +
 						"'>" +
-						format.resolution +
+						(format.format_note || format.resolution) +
 						"  |  " +
 						format.ext +
 						"  |  " +
@@ -157,13 +147,14 @@ async function getInfo(url) {
 				) {
 					size = size + " MB";
 					let audio_ext;
-					const format_id = format.format_id;
 
 					if (format.audio_ext === "webm") {
 						audio_ext = "opus";
 					} else {
 						audio_ext = format.audio_ext;
 					}
+
+					const format_id = format.format_id + "|" + audio_ext;
 
 					const element =
 						"<option value='" +
@@ -190,9 +181,9 @@ async function getInfo(url) {
 					console.log("filesize_approx " + format.filesize_approx);
 					const element =
 						"<option value='" +
-						format.format_id +
+						(format.format_id + "|" + format.ext) +
 						"'>" +
-						format.resolution +
+						(format.format_note || format.resolution) +
 						"  |  " +
 						format.ext +
 						"  |  " +
@@ -224,35 +215,49 @@ getId("audioDownload").addEventListener("click", (event) => {
 	download("audio");
 });
 
+//////////////////////////////
 // Downloading with ytdl
+//////////////////////////////
+
 function download(type) {
 	const url = getId("url").value;
+	let ext;
 
 	let format_id;
+	const randomId = Math.random().toFixed(10).toString().slice(2);
 
 	if (type === "video") {
-		format_id = getId("videoFormatSelect").value;
+
+		format_id = getId("videoFormatSelect").value.split("|")[0];
+		ext = getId("videoFormatSelect").value.split("|")[1];
+
 	} else {
-		format_id = getId("audioFormatSelect").value;
+
+		format_id = getId("audioFormatSelect").value.split("|")[0];
+		if (getId("audioFormatSelect").value.split("|")[1] === "webm") {
+			ext = "opus";
+		} else {
+			ext = getId("audioFormatSelect").value.split("|")[1];
+		}
 	}
 
 	const newItem = `
-		<div class="item" id="${newFolderName}">
-		<img src="../assets/images/close.png" onClick="fadeItem('${newFolderName}')" class="itemClose"}" id="${
-		newFolderName + ".close"
+		<div class="item" id="${randomId}">
+		<img src="../assets/images/close.png" onClick="fadeItem('${randomId}')" class="itemClose"}" id="${
+		randomId + ".close"
 	}">
-		<img src="https://img.youtube.com/vi/${id}/mqdefault.jpg" alt="thumbnail" class="itemIcon">
+		<img src="${thumbnail}" alt="thumbnail" class="itemIcon">
 
 		<div class="itemBody">
 			<div class="itemTitle">${title}</div>
 			<div class="itemType">${type}</div>
 			<input disabled type="range" value="0" class="hiddenVideoProgress" id="${
-				newFolderName + "vid"
+				randomId + "vid"
 			}"></input>
 			<input disabled type="range" value="0" class="hiddenAudioProgress" id="${
-				newFolderName + "aud"
+				randomId + "aud"
 			}"></input>
-			<div id="${newFolderName + "prog"}" class="itemProgress"></div>
+			<div id="${randomId + "prog"}" class="itemProgress"></div>
 		</div>
 	</div>
 	`;
@@ -260,19 +265,63 @@ function download(type) {
 	getId("loadingWrapper").style.display = "none";
 	let cancelled = false;
 
-	getId(newFolderName + ".close").addEventListener("click", () => {
-		if (getId(newFolderName)) {
-			fadeItem(newFolderName);
+	getId(randomId + ".close").addEventListener("click", () => {
+		if (getId(randomId)) {
+			fadeItem(randomId);
 		}
-		cancelled = true;
 	});
 
 	let downloadProcess;
-	if (type === "video") {
-		// cp.spawn(`yt-dlp -f ${format_id}+best`);
-		downloadProcess =  cp.spawn("yt-dlp", ['-f', `${format_id}+ba`])
-	} else {
+	let filename = "";
+	// Trying to remove ambiguous characters
+	for (let i = 0; i < title.length; i++) {
+		const pattern = /^[`~!@#$%^&*:;,<>?/|'"-+=\]\[]$/g;
+		let letter = "";
+		if (pattern.test(title[i])) {
+			letter = "";
+		} else {
+			if (title[i] == " ") {
+				letter = "_";
+			} else {
+				letter = title[i];
+			}
+		}
+		filename += letter;
 	}
+	filename += filename + `.${ext}`;
+	console.log(path.join(downloadDir, filename));
+	if (type === "video" && onlyvideo) {
+		// If video has no sound, audio needs to be downloaded
+		console.log("Downloading both video and audio");
+
+		downloadProcess = ytdlp.exec([
+			url,
+			"-f",
+			format_id + "+ba",
+			"-o",
+			`${path.join(downloadDir, filename)}`,
+		]);
+
+		// If downloading only audio or video with audio
+	} else {
+		downloadProcess = ytdlp.exec([
+			url,
+			"-f",
+			format_id,
+			"-o",
+			`${path.join(downloadDir, filename)}`,
+		]);
+	}
+
+	downloadProcess
+		.on("progress", (progress) => {
+			getId(
+				randomId + "prog"
+			).textContent = `Progress: ${progress.percent}%  Speed: ${progress.currentSpeed}`;
+		})
+		.on("close", () => {
+			afterSave(downloadDir, filename, randomId + "prog");
+		});
 }
 
 // Removing item
