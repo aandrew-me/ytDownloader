f09b77726e505629f63d3d8ee867d85775e1d5cd
aandrew-me <ahammadandrew@protonmail.com>
2022-08-31 12:35:12 +0600

    Fixing scripts

--- linux.sh
+++ linux.sh
@@ -2,6 +2,7 @@
 # Script to download the latest x64 linux version of custom ffmpeg build for yt-dlp
 # The binary will be placed in the root dir of the app
 
+rm ffmpeg.exe
 wget "https://github.com/yt-dlp/FFmpeg-Builds/releases/latest/download/ffmpeg-n5.1-latest-linux64-gpl-5.1.tar.xz"
 tar xvf ffmpeg-n5.1-latest-linux64-gpl-5.1.tar.xz
 cp ffmpeg-n5.1-latest-linux64-gpl-5.1/bin/ffmpeg ffmpeg
--- windows.sh
+++ windows.sh
@@ -2,10 +2,10 @@
 # Script to download the latest x64 windows version of custom ffmpeg build for yt-dlp
 # The binary will be placed in the root dir of the app
 
-
+rm ffmpeg
 wget "https://github.com/yt-dlp/FFmpeg-Builds/releases/download/latest/ffmpeg-n5.1-latest-win64-gpl-5.1.zip"
-tar xvf ffmpeg-n5.1-latest-linux64-gpl-5.1.tar.xz
-cp ffmpeg-n5.1-latest-linux64-gpl-5.1/bin/ffmpeg ffmpeg.exe
+unzip ffmpeg-n5.1-latest-win64-gpl-5.1.zip
+cp ffmpeg-n5.1-latest-win64-gpl-5.1/bin/ffmpeg.exe ffmpeg.exe
 chmod 777 ffmpeg.exe
-rm -rf ffmpeg-n5.1-latest-linux64-gpl-5.1
-rm ffmpeg-n5.1-latest-linux64-gpl-5.1.tar.xz
+rm -rf ffmpeg-n5.1-latest-win64-gpl-5.1
+rm ffmpeg-n5.1-latest-win64-gpl-5.1.zip
