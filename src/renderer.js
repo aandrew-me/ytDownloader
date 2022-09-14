432402dba9c57b7eb1b2bfc0dd32bc611124b801
aandrew-me <ahammadandrew@protonmail.com>
2022-09-14 10:56:29 +0600

    Limiting file name length

--- src/renderer.js
+++ src/renderer.js
@@ -558,7 +558,7 @@ function download(type) {
 		}
 		filename += letter;
 	}
-	filename = filename + Math.random().toFixed(3).toString().slice(2);
+	filename = filename.slice(0,100) + Math.random().toFixed(3).toString().slice(2);
 
 	let audioFormat;
 
