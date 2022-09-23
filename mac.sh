decf14d28f947ccd3eacfe2ae2b3803393a84ad3
aandrew-me <ahammadandrew@protonmail.com>
2022-09-23 10:43:53 +0600

    Build script changes

--- mac.sh
+++ mac.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # Script to download the latest x64 mac version of ffmpeg
 # The binary will be placed in the root dir of the app
-rm ffmpeg
-rm ffmpeg.exe
+rm ffmpeg*
 wget https://evermeet.cx/ffmpeg/ffmpeg-5.1.1.7z
 7z e ffmpeg-5.1.1.7z
 chmod 777 ffmpeg
--- windows.sh
+++ windows.sh
@@ -2,7 +2,7 @@
 # Script to download the latest x64 windows version of custom ffmpeg build for yt-dlp
 # The binary will be placed in the root dir of the app
 
-rm ffmpeg
+rm ffmpeg*
 wget "https://github.com/yt-dlp/FFmpeg-Builds/releases/download/latest/ffmpeg-n5.1-latest-win64-gpl-5.1.zip"
 unzip ffmpeg-n5.1-latest-win64-gpl-5.1.zip
 cp ffmpeg-n5.1-latest-win64-gpl-5.1/bin/ffmpeg.exe ffmpeg.exe
