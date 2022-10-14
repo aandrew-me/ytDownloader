74b6b8ee826641a17d2f9d1c8200c0e1629682a3
Oskar Vidarsson <oskarvidarsson@gmail.com>
2022-10-14 19:07:53 +0200

    changed to +x from 777
    Don't you just want to make it an executable instead of giving every user and group every permission there is?

--- linux.sh
+++ linux.sh
@@ -6,6 +6,6 @@ rm ffmpeg*
 wget "https://github.com/yt-dlp/FFmpeg-Builds/releases/latest/download/ffmpeg-n5.1-latest-linux64-gpl-5.1.tar.xz"
 tar xvf ffmpeg-n5.1-latest-linux64-gpl-5.1.tar.xz
 cp ffmpeg-n5.1-latest-linux64-gpl-5.1/bin/ffmpeg ffmpeg
-chmod 777 ffmpeg
+chmod +x ffmpeg
 rm -rf ffmpeg-n5.1-latest-linux64-gpl-5.1
 rm ffmpeg-n5.1-latest-linux64-gpl-5.1.tar.xz
