9a87c2dc1850bb20e719bbe1bb224c1c33fece63
aandrew-me <aandrew.me@proton.me>
2026-08-18 22:50:07 +0300

    Add windows arm64 build, download nodejs bins from nodejs.org

--- windows.ps1
+++ windows.ps1
@@ -1,4 +1,4 @@
-Invoke-WebRequest -Uri "https://github.com/aandrew-me/ffmpeg-builds/releases/download/v8/node.exe" -OutFile node.exe
+curl.exe -fsSL "https://nodejs.org/dist/latest-v22.x/win-x64/node.exe" -o node.exe
 Invoke-WebRequest -Uri "https://github.com/aandrew-me/ffmpeg-builds/releases/download/v8/ffmpeg_win64.zip" -OutFile ffmpeg_win64.zip
 Expand-Archive -Path ffmpeg_win64.zip -DestinationPath .
 Remove-Item -Path ffmpeg_win64.zip
